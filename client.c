316b7886c415c57473dfcb8fbc985645ffe0a590
Flayerman <127154686+Flayerman@users.noreply.github.com>
2024-01-04 22:10:48 +0100

    Create client.c
    Version 1

---
+++ client.c
@@ -0,0 +1,25 @@
+#include <stdio.h> /* librairie de base*/
+#include <stdlib.h> /* fonction exit*/
+#include <unistd.h> /*fonction pause / exit*/
+#include <signal.h> /* fonction signal */
+#include <string.h> /* autorisé*/
+
+
+
+
+int main(int argc, char *argv[])
+{
+	// Récupération du PID Server
+	pid_t server_pid = atoi(argv[1]);
+	printf("Sendind to %d\n",server_pid );
+	// Envoi de l'instruction SIGUSR1
+	//kill(server_pid,SIGUSR1);
+	// Envoie de la chaine de caractères
+	union sigval value;
+    value.sival_ptr = argv[2]; //stock la variable dans un adresse mémoire
+    sigqueue(server_pid, SIGUSR1, value);
+    printf("Message envoyé au serveur : %s\n", argv[2]);
+	printf("Done.\n");
+
+	return 0;
+}
