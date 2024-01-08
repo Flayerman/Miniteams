453fccd681226d307f4d3df8cbd0a738faab231f
Flayerman <127154686+Flayerman@users.noreply.github.com>
2024-01-08 20:17:01 +0100

    Update v2 client.c
    La fonction à 3 arguments est retirées.
    Les messages sont transmis en binaire avec USR1 et USR2.
    Il traduit ensuite le message de nouveaux en texte.
    ==========================================
    Il serait intéressant que le client envoi le nombre de caractère qu'il envoi afin d'allouer la mémoire nécessaire et traduire le message d'une traite.

--- client.c
+++ client.c
@@ -1,25 +1,61 @@
-#include <stdio.h> /* librairie de base*/
-#include <stdlib.h> /* fonction exit*/
-#include <unistd.h> /*fonction pause / exit*/
-#include <signal.h> /* fonction signal */
-#include <string.h> /* autorisé*/
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
+// char* charToBinary(char c)
+// {
+//     char* binaryString = (char*)malloc(9);
+//     for (int i = 7; i >= 0; --i) {
+//         binaryString[7 - i] = ((c >> i) & 1) + '0';
+//     }
 
+//     binaryString[8] = '\0';
+//     return binaryString;
+// }
 
-
-int main(int argc, char *argv[])
+void sendSignal(int bit, char* targetPID)
 {
-	// Récupération du PID Server
-	pid_t server_pid = atoi(argv[1]);
-	printf("Sendind to %d\n",server_pid );
-	// Envoi de l'instruction SIGUSR1
-	//kill(server_pid,SIGUSR1);
-	// Envoie de la chaine de caractères
-	union sigval value;
-    value.sival_ptr = argv[2]; //stock la variable dans un adresse mémoire
-    sigqueue(server_pid, SIGUSR1, value); // Il ne semble pas etre cable d'envoyer une chaine de caractère directement. Je dois convertir en binaire ?
-    printf("Message envoyé au serveur : %s\n", argv[2]);
-	printf("Done.\n");
+    int pid = atoi(targetPID);
 
-	return 0;
+    if (bit == 2) {
+        kill(pid, SIGTRAP);
+    }
+
+    else if (bit == 1) {
+        kill(pid, SIGUSR1);
+    } 
+    
+    else if (bit == 0) {
+        kill(pid, SIGUSR2);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3){
+        printf("Please only provide target PID and message to send.");
+        return 1;
+    }
+
+    char *targetPID = argv[1];
+    char *message = argv[2];
+    
+
+    printf("Input : %s\n", message);
+    printf("Converted: \n");
+
+    for (int i = 0; message[i] != '\0'; ++i) {
+        for (int j = 7; j >= 0; --j) {
+            printf("%d", (message[i] >> j) & 1);
+            sendSignal((message[i] >> j) & 1, targetPID);;
+            usleep(1000);
+        }
+        printf(" ");
+            printf("%s\n", targetPID);
+    }
+
+    sendSignal(2, targetPID);
+    printf("PID du processus : %d\n", getpid());
 }
